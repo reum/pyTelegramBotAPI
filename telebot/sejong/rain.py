63c219d0521a8985d29f7a6780364c36c46d15a5
sweetchip <sweetchip@sweetchip.kr>
2016-11-28 23:13:27 +0900

    modified rain.py (by @daeunim)

--- telebot/sejong/rain.py
+++ telebot/sejong/rain.py
@@ -1,36 +1,39 @@
+#-*- coding:utf-8 -*-
+import requests
 import re
 import xml.etree.ElementTree as ET
-import requests
-import urllib.request
-
-from urllib.request import urlopen
-
-url = "http://www.kma.go.kr//wid/queryDFSRSS.jsp?zone=1121573000"
-
-data = requests.get(url)
-	
-tree = ET.parse(urlopen('http://www.kma.go.kr//wid/queryDFSRSS.jsp?zone=1121573000'))
-root = tree.getroot()
+import urllib2
 
 def weather():
-	x = 0
-	n = []
-	for data in root.iter("data"):
-		if data.findtext("day")<'1' and data.findtext("pop")>='0' and data.findtext("pop")<='10':
-			n.append(int(data.findtext("pop")))
-			x = 1
-		elif data.findtext("day")<'1' and data.findtext("pop")>='11' and data.findtext("pop")<='50':
-			n.append(int(data.findtext("pop")))
-			x = 2
-		elif data.findtext("day")<'1' and data.findtext("pop")>='51' and data.findtext("pop")<='100':
-			n.append(int(data.findtext("pop")))
-			x = 3	
-	if x == 1 :
-		print("°­¼öÈ®·ü :",max(n),"%")
-		print("ºñ¾È¿Â´ë")
-	elif x == 2 :
-		print("°­¼öÈ®·ü :",max(n),"%")
-		print("ºñ¿ÃÁöµµ")
-	elif x == 3 :
-		print("°­¼öÈ®·ü :",max(n),"%")
-		print("¿ì»êÃ¬±â·Å")
+   try:
+      url = "http://www.kma.go.kr//wid/queryDFSRSS.jsp?zone=1121573000"
+      data = ET.ElementTree(file=urllib2.urlopen(url))
+      root = data.getroot()
+
+   except Exception as e :
+      error = "Error!!"
+      return error   
+
+   rain_prob = []
+   x = 0
+   n = []
+   
+   for data in root.iter("data"):
+      if data.findtext("day")<'1' and data.findtext("pop")>='0' and data.findtext("pop")<='10':
+         n.append(int(data.findtext("pop")))
+         x = 1
+      elif data.findtext("day")<'1' and data.findtext("pop")>='11' and data.findtext("pop")<='50':
+         n.append(int(data.findtext("pop")))
+         x = 2
+      elif data.findtext("day")<'1' and data.findtext("pop")>='51' and data.findtext("pop")<='100':
+         n.append(int(data.findtext("pop")))
+         x = 3   
+   if x == 1 :
+      rain_prob = "ê°•ìˆ˜í™•ë¥  : %s%% ë¹„ ì•ˆ ì˜¨ëŒ€"%max(n)
+   elif x == 2 :
+      rain_prob = "ê°•ìˆ˜í™•ë¥  : %s%% ë¹„ì˜¬ ê²ƒ ê°™ì•„"%max(n)
+   elif x == 3 :
+      rain_prob = "ê°•ìˆ˜í™•ë¥  : %s%% ìš°ì‚° ì±™ê¸°ë ´"%max(n)
+   
+   return rain_prob
+

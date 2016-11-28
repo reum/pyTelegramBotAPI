23fa75f264c099be6778c62ea415f14fdb1d140f
Seung-Hyeon Cho <lustro@live.co.kr>
2016-11-28 21:03:17 +0900

    Redo merged

--- examples/sejong/server.py
+++ examples/sejong/server.py
@@ -1,89 +1,77 @@
-﻿# -*- coding: utf-8 -*-
-import sys
-import os
-import random
+﻿#-*- coding: utf-8 -*-
+import sys, os
 
 try:
     import telebot
 except ImportError:
-    sys.path.append("../../")
     sys.path.append(os.getcwd())
     import telebot
-
-    from telebot.sejong import easteregg
-    from telebot.sejong import volunteer
-    from telebot.sejong import cvesearch
-    from telebot.sejong import studyroom
-    from telebot.sejong import news
-    from telebot import types
+    from telebot.sejong import *
 
 try:
     from api_token import API_TOKEN
 except ImportError as e:
-	API_TOKEN = '<api_token>'
+    API_TOKEN = '<api_token>'
 
-   
-#################
 bot = telebot.TeleBot(API_TOKEN)
-#################
 
-@bot.message_handler(commands=['start', 'help'])
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
 
+# Handle '/sroom' 
+@bot.message_handler(commands=['sroom'])
+def search_sroom(message):
+    p = utils.Parser(" ".join(message.text.split(" ")[1:]))
 
-@bot.message_handler(commands=['iu'])
-def send_easteregg(message):
-    i = random.randint(0, 1)
-    if i == 0:
-        fname_photo = iu_insta.getImage()
-        photo = open('./insta_images/'+fname_photo, 'rb')
-        bot.send_photo(message.chat.id, photo)
+    p.setAssum(int, "year", ["-y", "y"])
+    p.setAssum(int, "month", ["-m", "m"])
+    p.setAssum(int, "date", ["-d", "d"])
+    p.setAssum(str, "time", ["-t", "t"])
+
+    p.setType(int,0)
+    p.setType(int,1)
+    p.setType(int,2)
+
+    rs = studyroom.RoomStatus.instance()
+    """if len(p) == 0:
+        pass
+    elif len(p) == 2: # date, time
+        pass
+    elif len(p) == 3: # month, date, time
+        pass
+    elif len(p) == 4: # year, month, date, time
+        pass
     else:
-        youtube = iu_youtube.getIUYoutube()
-        youtube_str = u"오늘의 음악추천 : "+youtube['title']+ u"\nURL :"+ youtube['url']
-        bot.reply_to(message, youtube_str)
+    """
+    if '~' in p[3] :
+        s, e = p[3].split('~')
+    elif '-' in p[3] :
+        s, e = p[3].split('-')
+    else:
+        s = e = p[3]
+
+    try:
+        s = int(s)
+        e = int(e)
+        rst = rs.mappingResult(rs.search(p[0],p[1],p[2],range(s,e+1)))
+        bot.reply_to(message, ", ".join(rst))
+        print rst
+    except:
+        """try:
+            rst = rs.search(p["year"], p["month"], p["date"], p["time"])
+        except:
+           """ 
+        bot.reply_to(message, "Error!! %s" %(message.text,))
 
 
-@bot.message_handler(commands=['vol'])
-def send_volunteerinfo(message):
-    chat_id = message.chat.id
-    markup = types.ReplyKeyboardMarkup()
-    itembtna = types.KeyboardButton(u'/외부봉사')
-    itembtnv = types.KeyboardButton(u'/내부봉사')
-    markup.row(itembtna, itembtnv)
-    bot.send_message(chat_id, "Choose volunteer:", reply_markup=markup)
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
-@bot.message_handler(func=lambda message: message.text == u'/내부봉사' and message.content_type == 'text')
-def send_volunteerinfo(message):
-    chat_id = message.chat.id
-    result = ""
-    volInternal = volunteer.getVolunteerInternal()
-    for vol in volInternal:
-        result += "="*20+"\n"
-        result += u"봉사 이름 :"+vol['title'] +"\n"
-        result += u"봉사 기간 :"+vol['date']+u"("+vol['day']+u")"+"\n"
-        result += u"봉사 시간 :"+vol['time']+"\n"
-    bot.reply_to(message, result)
-
-@bot.message_handler(func=lambda message: message.text == u'/외부봉사' and message.content_type == 'text')
-def send_volunteerinfo(message):
-    chat_id = message.chat.id
-    result = ""
-    volInternal = volunteer.getVolunteerExternal()
-    for vol in volInternal:
-        result += u"봉사 이름 :"+vol['title']+"\n"
-    bot.reply_to(message, result)
-
-#@bot.message_handler(func=lambda message: True)
-#def echo_all(message):
-#    bot.reply_to(message, message.text)
-#    print message
-
-if __name__ == '__main__':
-    iu_insta = easteregg.Insta("dlwlrma")
-    iu_youtube = easteregg.IUYoutube()
-    iu_youtube.setJsonFile("IU_playlist.json")
-
-    bot.polling(none_stop=False, interval=1)
-
+bot.polling()

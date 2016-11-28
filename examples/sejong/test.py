20406084d6bd913467aef573524654ea5967df1f
Seung-Hyeon Cho <lustro@live.co.kr>
2016-11-28 12:25:23 +0900

    Added Parser test code

--- examples/sejong/test.py
+++ examples/sejong/test.py
@@ -7,13 +7,28 @@ except ImportError:
     sys.path.append(os.getcwd())
     import telebot
 
+    from telebot.sejong import utils
     from telebot.sejong import easteregg
     from telebot.sejong import volunteer
     from telebot.sejong import studyroom
 
+    
+# Parser Test
+
+a = utils.Parser("-y 2016 -m 11 -d 22")
+
+a.setAssum(int, "date", ["-d", "d", "date"])
+
+print a["date"]
+
+b = utils.Parser("2016 11 22")
+print type(b[0]), b[0]
+b.setType(int, 0)
+print type(b[0]), b[0]
+
+
 print easteregg.crawlInsta()
 print volunteer.getVolunteerInternal()
-
 
 # Study room search
 rs = studyroom.RoomStatus.instance()
@@ -34,3 +49,4 @@ print rs.search(2016,10,12,range(10, 10+4))
 
 # usage 3
 print rs.mappingResult(rs.search(2016,10,12,range(10, 10+4)))
+
